2dcba2a4880f9070e75682b12dbe39b65564837d
Venera <veneraplakolli252@gmail.com>
2024-12-13 16:13:30 +0100

    Programming Quiz App with React Js

---
+++ src/questions.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    id: "q1",
+    text: "Which of the following CSS layout models allows for the creation of complex responsive layouts, particularly with dynamic resizing of elements?",
+    answers: [
+      "CSS Grid",
+      "Flexbox",
+      "Float-based layout",
+      "Table-based layout",
+    ],
+  },
+  {
+    id: "q2",
+    text: "In JavaScript, which method would you use to prevent an event from bubbling up to parent elements in the DOM?",
+    answers: [
+      "event.stopPropagation()",
+      "event.preventDefault()",
+      "event.cancelBubble",
+      "event.stopImmediatePropagation()",
+    ],
+  },
+  {
+    id: "q3",
+    text: "Which of the following JavaScript frameworks is specifically designed to build single-page applications (SPAs) by creating a virtual DOM for efficient updates?",
+    answers: ["React", "Angular", "Vue.js", "Ember.js"],
+  },
+  {
+    id: "q4",
+    text: "Can you identify what JSX is?",
+    answers: [
+      "A JavaScript extension that adds HTML-like syntax to JavaScript.",
+      "A JavaScript library for building dynamic user interfaces.",
+      "A specific HTML version that was explicitly created for React.",
+      "A tool for making HTTP requests in a React application.",
+    ],
+  },
+  {
+    id: "q5",
+    text: "What is the primary purpose of the async and await keywords in modern JavaScript?",
+    answers: [
+      "To handle asynchronous operations in a more readable and synchronous-looking way",
+      "To define anonymous functions",
+      "To create reusable code blocks in JavaScript",
+      "To optimize code execution for performance",
+    ],
+  },
+  {
+    id: "q6",
+    text: "Which of the following is the correct syntax for creating a flex container in CSS that aligns children in a row and centers them vertically?",
+    answers: [
+      "display: flex; justify-content: center; align-items: center;",
+      "display: flex; align-content: center; justify-items: center;",
+      "display: flex; flex-direction: row; align-items: flex-start;",
+      "display: grid; justify-items: center; align-items: center;",
+    ],
+  },
+  {
+    id: "q7",
+    text: "Which of the following JavaScript methods would you use to add a new element to the end of an array?",
+    answers: [
+      "array.push()",
+      "array.shift()",
+      "array.unshift()",
+      "array.splice()",
+    ],
+  },
+  {
+    id: "q8",
+    text: "In CSS, what is the purpose of the z-index property?",
+    answers: [
+      "To control the stacking order of elements on the page",
+      "To apply a shadow effect on elements",
+      "To define the horizontal positioning of elements",
+      "To create an animation for elements",
+    ],
+  },
+];
